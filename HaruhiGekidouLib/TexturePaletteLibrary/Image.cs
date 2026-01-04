f9413e8e41bddf639bc5ded6b23183de71dfd4d8
Lolon <Lolonmedia@gmail.com>
2026-01-04 11:05:51 +0000

    removed cmpr

--- HaruhiGekidouLib/TexturePaletteLibrary/Image.cs
+++ HaruhiGekidouLib/TexturePaletteLibrary/Image.cs
@@ -61,105 +61,40 @@ public class Image
 
         bitmap = new SKBitmap((int)width, (int)height);
 
-        if (format == ImageFormat.CI8)
+        switch (format)
         {
-            int ci8Index = (int)imageDataaddress;
-            for (int y = 0; y < height; y += 4) //each pixel row
+            case ImageFormat.CI8:
             {
-                int widthMod = (8 - (width % 8)) == 8 ? 0 : 8 - (width % 8);
-                for (int x = 0; x < width + widthMod; x += 8)   //each pixel column
+                int ci8Index = (int)imageDataaddress;
+                for (int y = 0; y < height; y += 4) //each pixel row
                 {
-                    for (int row = 0; row < 4; row++)
+                    int widthMod = (8 - (width % 8)) == 8 ? 0 : 8 - (width % 8);
+                    for (int x = 0; x < width + widthMod; x += 8)   //each pixel column
                     {
-                        for (int col = 0; col < 8; col++)
+                        for (int row = 0; row < 4; row++)
                         {
-                            if (ci8Index + 1 >= data.Length || x + col >= width || y + row >= height)
+                            for (int col = 0; col < 8; col++)
                             {
-                                ci8Index += 1;
-                                continue;
-                            }
-
-                            byte colorByte = data[ci8Index];
-                            int colIndex = colorByte;
-                            SKColor color = colPalette.Colors[colIndex];
-                            bitmap.SetPixel(x + col, y + row, color);
-                            ci8Index += 1; 
-                        }
-                    }
-                }
-            }
-        }
-        else if (format == ImageFormat.CMPR)
-        {
-            int cmprIndex = (int)imageDataaddress;
-            for (int y = 0; y < height; y += 8)
-            {
-                int widthMod = (8 - (width % 8)) == 8 ? 0 : 8 - (width % 8);
-                for (int x = 0; x < width + widthMod; x += 8)
-                {
-                    // 8x8 bytes, 4x4 sub-blocks
-                    for (int row = 0; row < 8; row += 4)
-                    {
-                        for (int col = 0; col < 8; col += 4)
-                        {
-                            if (cmprIndex >= data.Length)
-                            {
-                                break;
-                            }
-
-                            ushort[] paletteData =
-                            [
-                                BitConverter.ToUInt16(data, cmprIndex),
-                                BitConverter.ToUInt16(data, cmprIndex + 2),
-                            ];
-                            var palette = new SKColor[4];
-                            for (int i = 0; i < paletteData.Length; i++)
-                            {
-                                palette[i] = new((byte)(((paletteData[i] >> 11) & 0x1F) * 0x08),
-                                    (byte)(((paletteData[i] >> 5) & 0x3F) * 0x04),
-                                    (byte)((paletteData[i] & 0x1F) * 0x08), 0xFF);
-                            }
-
-                            if (paletteData[0] > paletteData[1])
-                            {
-                                palette[2] = new((byte)((palette[0].Red * 2 + palette[1].Red) / 3),
-                                    (byte)((palette[0].Green * 2 + palette[1].Green) / 3),
-                                    (byte)((palette[0].Blue * 2 + palette[1].Blue) / 3), 0xFF);
-                                palette[3] = new((byte)((palette[0].Red + palette[1].Red * 2) / 3),
-                                    (byte)((palette[0].Green + palette[1].Green * 2) / 3),
-                                    (byte)((palette[0].Blue + palette[1].Blue * 2) / 3), 0xFF);
-                            }
-                            else
-                            {
-                                palette[2] = new((byte)((palette[0].Red + palette[1].Red) / 2),
-                                    (byte)((palette[0].Green + palette[1].Green) / 2),
-                                    (byte)((palette[0].Blue + palette[1].Blue) / 2), 0xFF);
-                                palette[3] = SKColors.Transparent;
-                            }
-
-                            cmprIndex += 4;
-                            for (int subRow = 0; subRow < 4; subRow++)
-                            {
-                                byte pixelData = data[cmprIndex++];
-                                for (int subCol = 0; subCol < 4; subCol++)
+                                if (ci8Index + 1 >= data.Length || x + col >= width || y + row >= height)
                                 {
-                                    if (x + col + subCol >= width || y + row + subRow >= height)
-                                    {
-                                        continue;
-                                    }
-
-                                    bitmap.SetPixel(x + col + subCol, y + row + subRow,
-                                        palette[(pixelData >> (2 * (3 - subCol))) & 0x03]);
+                                    ci8Index += 1;
+                                    continue;
                                 }
+
+                                byte colorByte = data[ci8Index];
+                                int colIndex = colorByte;
+                                SKColor color = colPalette.Colors[colIndex];
+                                bitmap.SetPixel(x + col, y + row, color);
+                                ci8Index += 1; 
                             }
                         }
                     }
                 }
+
+                break;
             }
-        }
-        else
-        {
-            throw new Exception("Image format "+ format.ToString() + " not supported");
+            default:
+                throw new Exception("Image format "+ format.ToString() + " not supported");
         }
     }
 
@@ -172,37 +107,44 @@ public class Image
         bytes.AddRange(IO.GetUIntBytes((uint)format));
         
         List<byte> data = new List<byte>();
-        if (format == ImageFormat.CI8)
+        switch (format)
         {
-            int ci8Index = 0;
-            int heightMod = (4 - (height % 4)) == 4 ? 0 : 4 - (width % 4);
-            for (int y = 0; y < height+ heightMod; y += 4) //each pixel row
+            case ImageFormat.CI8:
             {
-                int widthMod = (8 - (width % 8)) == 8 ? 0 : 8 - (width % 8);
-                for (int x = 0; x < width + widthMod; x += 8)   //each pixel column
+                int ci8Index = 0;
+                int heightMod = (4 - (height % 4)) == 4 ? 0 : 4 - (width % 4);
+                for (int y = 0; y < height+ heightMod; y += 4) //each pixel row
                 {
-                    for (int row = 0; row < 4; row++)
+                    int widthMod = (8 - (width % 8)) == 8 ? 0 : 8 - (width % 8);
+                    for (int x = 0; x < width + widthMod; x += 8)   //each pixel column
                     {
-                        for (int col = 0; col < 8; col++)
+                        for (int row = 0; row < 4; row++)
                         {
-                            if (x + col >= width || y + row >= height)
+                            for (int col = 0; col < 8; col++)
                             {
+                                if (x + col >= width || y + row >= height)
+                                {
+                                    ci8Index += 1;
+                                    continue;
+                                }
+
+                                SKColor color = bitmap.GetPixel(x + col, y + row);
+                                int iPaletteIndex = int.Clamp(assignedPalette.Colors.IndexOf(color), 0, 255);
+                                byte paletteindex = Convert.ToByte(iPaletteIndex);
+                                data.Add(paletteindex);
+
+                                //Data[ia8Index] = color.Alpha;
+                                // Data[ia8Index + 1] = (byte)((color.Red / 3) + (color.Blue / 3) + (color.Green / 3));
                                 ci8Index += 1;
-                                continue;
                             }
-
-                            SKColor color = bitmap.GetPixel(x + col, y + row);
-                            int iPaletteIndex = int.Clamp(assignedPalette.Colors.IndexOf(color), 0, 255);
-                            byte paletteindex = Convert.ToByte(iPaletteIndex);
-                            data.Add(paletteindex);
-
-                            //Data[ia8Index] = color.Alpha;
-                           // Data[ia8Index + 1] = (byte)((color.Red / 3) + (color.Blue / 3) + (color.Green / 3));
-                           ci8Index += 1;
                         }
                     }
                 }
+
+                break;
             }
+            default:
+                throw new Exception("Image format "+ format.ToString() + " not supported");
         }
 
         List<byte> restOfHeader = new List<byte>();
